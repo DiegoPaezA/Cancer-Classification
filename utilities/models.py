35428611441ca9038f858b74e5dc8b239e246a6c
diegopaezasus <diripar8@gmail.com>
2023-04-21 17:58:49 -0300

    working on run experiment

--- utilities/models.py
+++ utilities/models.py
@@ -13,8 +13,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
 
-from preprocessing import split_data, get_xy
+from utilities.preprocessing import split_data, get_xy
+from utilities.metrics import eval_performance
 
 
 def train_model(model, data, target, test_size=0.2):
@@ -35,9 +37,38 @@ def train_model(model, data, target, test_size=0.2):
   
   accuracy_kfold = round(mean(scores_origin),3)
   std_kfold = round(std(scores_origin),3)
+  
   model.fit(X_train, y_train)
   model_predic = model.predict(X_test)
 
   # ['accuracy', 'average_precision', 'f1','recall', 'roc_auc']
 
   return [accuracy_kfold, std_kfold, model_predic]
+
+def run_experiment(data, target, test_size=0.2):
+  """
+  This function runs the experiment with the models.
+  
+  Args:
+    data (pandas.DataFrame): The dataset to run the experiment on.
+    target (str): The name of the target column.
+    test_size (float): The size of the test set.
+  Returns:
+    names (list): The list of the names of the models.
+    results (list): The list of the results of the models that 
+                    includes the kfold accuracy, std, and predictions of trained models.
+    
+  """
+  models = []
+  models.append(('LR', LogisticRegression()))
+  models.append(('NB', GaussianNB()))
+  models.append(('SVM', SVC()))
+  models.append(('KNN', KNeighborsClassifier()))
+  models.append(('XGB', XGBClassifier()))
+
+  results = []
+  models_names = ["LR", "NB", "SVM", "KNN", "XGB"]
+  for model in models:
+    results.append(train_model(model, data, target, test_size))
+    
+  return results
