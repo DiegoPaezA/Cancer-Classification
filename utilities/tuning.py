1afc697b79377288aebc8ee9182a2af5177f0ff0
diegopaezasus <diripar8@gmail.com>
2023-04-22 16:12:13 -0300

    working...

--- utilities/tuning.py
+++ utilities/tuning.py
@@ -3,6 +3,7 @@ This module contains functions for tuning the hyperparameters of a model.
 """
 import yaml
 from sklearn.model_selection import GridSearchCV
+from utilities.preprocessing import split_data, get_xy, oversample_data
 
 def load_params(param_file):
     """
@@ -24,3 +25,20 @@ def tune_hyperparameters(model, parameters, X_train, y_train):
     grid_search = GridSearchCV(model, parameters, cv=5, scoring='accuracy')
     grid_search.fit(X_train, y_train)
     return grid_search.best_params_
+
+def tune_experiment(data,target, parameters):
+    """
+    Tune the hyperparameters of a model using GridSearchCV and return the 
+    accuracy score on the test set.
+    
+    Args:
+        data (pandas.DataFrame): the data
+        target (str): the name of the target column
+        parameters (dict): the parameters to tune
+    Returns:
+        tuned_params (dict): the tuned parameters
+    """
+    
+    grid_search = GridSearchCV(model, parameters, cv=5, scoring='accuracy')
+    grid_search.fit(X_train, y_train)
+    return grid_search.score(X_test, y_test)
